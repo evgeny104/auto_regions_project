21ff4257f697b0c707da3d51224b599def46020c
evgeny104 <nazawka88@gmail.com>
2024-04-12 12:54:11 +0300

    File added config.py for storing url and json-object

---
+++ config.py
@@ -0,0 +1,8 @@
+base_url = 'https://regions-test.2gis.com'
+
+error_country_code = {
+    'error': {
+        'id': '74a8e6e5-1539-4125-abfd-77c2db126f7c',
+        'message': "Параметр 'country_code' может быть одним из следующих значений: ru, kg, kz, cz"
+    }
+}
