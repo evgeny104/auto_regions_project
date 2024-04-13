d6ccebc6e73b1347b59958936d8925fa0f6ebe99
evgeny104 <nazawka88@gmail.com>
2024-04-13 23:05:28 +0300

    add test suite country_code

--- main.py
+++ main.py
@@ -1,38 +1,21 @@
-import json
 import requests
-from json_object import error_country_code
-
-'''def test_total_count():     # Функция проверяет -строку- "total", общее количество регионов в базе 22
-    res = requests.get('https://regions-test.2gis.com/1.0/regions?page_size=5')
-    body = json.loads(res.text)
-    assert body["total"] == 22
+from config import error_country_code, base_url
 
 
-def test_region_q():        # Функция проверяет поиск по наванию региона "Минимум -3 символа"
-    res = requests.get('https://regions-test.2gis.com/1.0/regions?q=тау')
-    body = json.loads(res.text)
-    assert (body["items"][0]["name"]) == "Актау"
-
-
-def test_region_q_register():        # Функция проверяет поиск по названию региона "Регистр не имеет значения"
-    res = requests.get('https://regions-test.2gis.com/1.0/regions?q=СквА')
-    body = json.loads(res.text)
-    assert (body["items"][0]["name"]) == "Москва"
-
-
-def test_region_q_allignore():      # Функция проверяет поиск по названию региона "все остальные параметры игнорируются"
-    res = requests.get('https://regions-test.2gis.com/1.0/regions?q=УфА&country_code=cz&page=1&page_size=15')
-    body = json.loads(res.text)
-    assert (body["total"]) == 22
-    assert (body["items"][0]["id"]) == 17
-    assert (body["items"][0]["name"]) == "Уфа"
-    assert (body["items"][0]["code"]) == "ufa"
-    assert (body["items"][0]["country"]["name"]) == "Россия"
-    assert (body["items"][0]["country"]["code"]) == "ru"'''
+def test_country_code_page():  # № Test №502,1 for Pytest
+    url = f"{base_url}/1.0/regions"
+    server_request = requests.get(url)
+    assert server_request.status_code == 200, "Failed to fetch regions data"
+    response_data = server_request.json()
+    urlpage = f"{base_url}/1.0/regions?page=1"
+    request = requests.get(urlpage)
+    assert request.status_code == 200, "Failed to fetch regions data for page 1"
+    data = request.json()
+    assert response_data == data
 
 
 def test_country_code_ru():  # № Test 502,2
-    url = 'https://regions-test.2gis.com/1.0/regions?country_code=ru'
+    url = f"{base_url}/1.0/regions?country_code=ru"
     request = requests.get(url)
     assert request.status_code == 200
     data = request.json()
@@ -42,7 +25,7 @@ def test_country_code_ru():  # № Test 502,2
 
 
 def test_country_code_kg():  # № Test 502,3
-    url = 'https://regions-test.2gis.com/1.0/regions?country_code=kg'
+    url = f"{base_url}/1.0/regions?country_code=kg"
     request = requests.get(url)
     assert request.status_code == 200
     data = request.json()
@@ -52,7 +35,7 @@ def test_country_code_kg():  # № Test 502,3
 
 
 def test_country_code_kz():  # № Test 502,4
-    url = 'https://regions-test.2gis.com/1.0/regions?country_code=kz'
+    url = f"{base_url}/1.0/regions?country_code=kz"
     request = requests.get(url)
     assert request.status_code == 200
     data = request.json()
@@ -62,7 +45,7 @@ def test_country_code_kz():  # № Test 502,4
 
 
 def test_country_code_cz():  # № Test 502,5
-    url = 'https://regions-test.2gis.com/1.0/regions?country_code=cz'
+    url = f"{base_url}/1.0/regions?country_code=cz"
     request = requests.get(url)
     assert request.status_code == 200
     data = request.json()
@@ -72,7 +55,7 @@ def test_country_code_cz():  # № Test 502,5
 
 
 def test_country_code_test():  # № Test 502,6
-    url = 'https://regions-test.2gis.com/1.0/regions?country_code=test'
+    url = f"{base_url}/1.0/regions?country_code=test"
     request = requests.get(url)
     assert request.status_code == 200
     data = request.json()
